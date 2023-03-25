fae049deb92eb98362599a654bbec0d1818bcf2c
vlad <vladislavludinov3006@gmail.com>
2023-03-25 11:09:24 +0300

    berries collection calculation is completed

--- Task24.py
+++ Task24.py
@@ -3,4 +3,12 @@ from random import randint
 
 bushes = [randint(0, 20) for _ in range(int(input("Введите количество кустов: ")))]
 
-сollect_berries = []
+collect_berries = []
+
+print(f"Количество ягод на кустах: {bushes}")
+
+for i in range(len(bushes)-1):
+    collect_berries.append(bushes[i-1]+bushes[i]+bushes[i+1])
+collect_berries.append(bushes[len(bushes)-2]+bushes[len(bushes)-1]+bushes[0])
+print(f"Возможные варианты сбора: {collect_berries}")
+
